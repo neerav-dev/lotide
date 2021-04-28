a748c12baca440929064cdf50f9e357eca3fcdfb
Neerav Patel <nickpatel.developer@gmail.com>
2021-04-28 13:23:03 -0400

    Middle function to find middle element of array

--- middle.js
+++ middle.js
@@ -2,14 +2,14 @@ let eqLength = function(array1, array2) {
   return array1.length === array2.length;
 };
 
-let isEmpty = function (array){
+let isEmpty = function(array) {
   return Array.isArray(array) && (array.length === 0);
 };
 
 let eqArrays = function(array1, array2) {
   let equals = false;
   
-  equals = (isEmpty(array1) && isEmpty(array2))  && true; 
+  equals = (isEmpty(array1) && isEmpty(array2))  && true;
 
   if (eqLength(array1, array2)) {
     for (let i = 0; i < array1.length; i++) {
