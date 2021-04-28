208b90f3b8f07f5d2ee81cf37dbda34790ee1852
Neerav Patel <nickpatel.developer@gmail.com>
2021-04-28 13:19:57 -0400

    Added empty array check to assertArray

--- eqArrays.js
+++ eqArrays.js
@@ -6,8 +6,15 @@ let eqLength = function(array1, array2) {
   return array1.length === array2.length;
 };
 
+let isEmpty = function (array){
+  return Array.isArray(array) && (array.length === 0);
+};
+
 let eqArrays = function(array1, array2) {
   let equals = false;
+  
+  equals = (isEmpty(array1) && isEmpty(array2))  && true; 
+
   if (eqLength(array1, array2)) {
     for (let i = 0; i < array1.length; i++) {
       equals = array1[i] === array2[i];
@@ -16,6 +23,8 @@ let eqArrays = function(array1, array2) {
   return equals;
 };
 
+// Comparing identical number array
+assertEqual(eqArrays([], []), true);
 // Comparing identical number array
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 // Comparing non-identical array
