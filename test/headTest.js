80ae8bf9f62fb1ca9c43112e7568050e80d1e850
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-03 13:23:27 -0400

    setup project with Mocha and Chai and converted headTest.js to use that instead

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,32 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
-// check with number array
-assertEqual(head([5, 6, 7]), 5);
-// check with string array
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// check for one element
-assertEqual(head(["one"]), "one");
-// check empty array
-assertEqual(head([]), undefined);
+
+describe('#head', () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  // check with number array
+  it('returns 5 for [5, 6, 7]', () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  // check with string array
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  // check for one element
+  it('returns "one" for ["one"]', ()=> {
+    assert.strictEqual(head(["one"]), "one");
+  });
+  
+  // check empty array
+  it('returns "undefined" for []', ()=>{
+    assert.strictEqual(head([]), undefined);
+  });
+});
